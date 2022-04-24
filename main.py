d04255938ed66bdca83936f17401dc7368f3ddb5
Ruben Toro <rubensdt01@gmail.com>
2022-04-24 06:45:05 -0500

    add user router, create user with hash pass

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
+from app.v1.router.user_router import router as user_router
+
 app = FastAPI()
 
-@app.get ('/')
-def home():
-    return {'message': 'Hello World'}
+
+app.include_router(user_router)
